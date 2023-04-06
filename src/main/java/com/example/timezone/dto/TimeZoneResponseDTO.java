3c079e6e2a44ff72b34cdacb101ab18b50a6d23a
BalReddy Yedla <yedlabalreddy1991@gmail.com>
2023-04-06 08:26:20 +0530

    Fixing temporary change

--- src/main/java/com/example/timezone/dto/TimeZoneResponseDTO.java
+++ src/main/java/com/example/timezone/dto/TimeZoneResponseDTO.java
@@ -9,19 +9,4 @@ public class TimeZoneResponseDTO {
     private String abbreviation;
     private String timezone;
     private String datetime;
-    
-     public TimeZoneResponseDTO(String abbreviation, String timezone, String datetime) {
-		super();
-		this.abbreviation = abbreviation;
-		this.timezone = timezone;
-		this.datetime = datetime;
-	}
-	public String getAbbreviation() {
-		return abbreviation;
-	}
-	public void setAbbreviation(String abbreviation) {
-		this.abbreviation = abbreviation;
-	}
-	public String getTimezone() {
-		return timezone;
 }
